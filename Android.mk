8ce22bb5dc4112932c7aa263b30ce68f5b736daf
Berger <berger@cs.rwth-aachen.de>
2021-04-22 12:08:21 +0200

    Fix missing gt510lte.

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gtelwifiue gtesqltespr gt5note10wifi,$(TARGET_DEVICE)),)
+ifneq ($(filter gtelwifiue gtesqltespr gt5note10wifi gt510lte,$(TARGET_DEVICE)),)
 
 endif
